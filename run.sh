018f4519feb647a33a1e4dd2b836aa34675646bb
f100x <frankjag@yandex.com>
2023-11-30 18:12:59 -0300

    update

--- run.sh
+++ run.sh
@@ -1,18 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-# Función para ejecutar run.sh cada 30 minutos
-run_every_3_hours() {
-    while true; do
-        # kill prev copy if exists
-        pkill -e -f wa
+# kill prev copy if exists
+pkill -e -f wa
 
-        # start a new one
-        ./wa &
 
-        # Espera 30 minutos antes de la próxima ejecución
-        sleep 28800
-    done
-}
-
-# Llama a la función para ejecutar run.sh cada 30 minutos
-run_every_3_hours
+# start a new one
+./wa &
